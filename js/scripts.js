87708ccafc3e747330585acaf490d88c95672eb1
Braden Nelson <connornel123@outlook.com>
2021-06-08 10:47:58 -0700

    add repository

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic//
+
+function elementcounter (array) {
+  let counter=0;
+  for(let i =0; i < array.length; i++) {
+  counter++
+  }
+  return counter
+}
+
+const info= ["hello", "world"];
